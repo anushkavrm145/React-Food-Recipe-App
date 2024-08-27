8c7f89df07d8018bd58ef63b2aa003b338a86315
anushkavrm145 <anushkavrm145@gmail.com>
2024-08-27 16:37:33 +0530

    first

---
+++ src/context/index.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const GlobalContext = createContext(null);
+export default function GlobalState({ children }) {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData,setRecipeDetailsData] = useState(null)
+  const [favoritesList,setFavoritesList]=useState([])
+  const navigate = useNavigate()
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+      const data = await res.json();
+      if (data?.data?.recipes){console.log(data?.data?.recipes);setRecipeList(data.data.recipes);}
+      setLoading(false);
+      setSearchParam("");
+      navigate('/')
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+      setSearchParam("");
+    }
+  }
+  function handleAddToFavorite(getCurrentItem){
+    console.log(getCurrentItem)
+    let cpyFavoriteList = [...favoritesList]
+    const index = cpyFavoriteList.findIndex(item=> item.id === getCurrentItem.id)
+    if(index === -1) cpyFavoriteList.push(getCurrentItem)
+    else cpyFavoriteList.splice(index)
+setFavoritesList(cpyFavoriteList)
+
+  }
+  console.log(favoritesList,'favoritesList')
+  return (
+    <GlobalContext.Provider value={{ 
+        searchParam,
+        loading,
+        recipeList,
+        setSearchParam,
+        handleSubmit,recipeDetailsData ,setRecipeDetailsData,handleAddToFavorite,favoritesList}}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
